13c41daccfdec9509e8ed8c2696ea8785e767852
Emantor <phoenix@emantor.de>
2016-12-28 16:39:37 +0100

    33c33, client3: Add python3 client

---
+++ python/33c33.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+
+import datetime
+import time
+import client
+import urllib2
+import json
+import logging
+from itertools import islice
+from collections import OrderedDict
+
+logging.basicConfig(level=logging.DEBUG)
+
+tick = 11
+
+def clear():
+    pixels = [0] * client.HEIGHT * client.WIDTH
+    client.blit(0,0,client.WIDTH,client.HEIGHT,pixels)
+
+def filter_time(talks):
+    for talk in talks:
+        p = datetime.datetime.strptime(talk["date"][0:-6],"%Y-%m-%dT%H:%M:%S")
+        if p > datetime.datetime.now():
+            return talk
+
+def fetch_scoreboard():
+    board_json = urllib2.urlopen("https://33c3ctf.ccc.ac/scoreboard.json")
+    return json.load(board_json)
+
+client.write(0,4,"{:<30}".format("CTF Scoreboard (first 5):"))
+
+
+while True:
+    d = datetime.datetime.now()
+    t = d.strftime(" %H:%M")
+    delta = d - datetime.datetime(2016,12,27)
+    logging.debug("Calculated delta.days: %s", delta)
+    if tick > 10:
+        j = urllib2.urlopen('https://fahrplan.events.ccc.de/congress/2016/Fahrplan/schedule.json')
+        schedule = json.load(j)
+        teams = fetch_scoreboard()
+        tick = 0
+        for i in range(5,10):
+            team = teams["standings"][i-5]
+            client.write(0,i,"{:>1}. {:<29} : {:<4}     ".format(team["pos"],team["team"],team["score"]))
+    if schedule:
+        saale = ["Saal 1", 'Saal 2', "Saal G", "Saal 6"]
+        talks = OrderedDict()
+        for saal in saale:
+            talks[saal] = schedule['schedule']['conference']['days'][delta.days]['rooms'][saal] 
+        i = 0
+        f_talk = OrderedDict()
+        for saal in talks:
+             f_talk[saal] = filter_time(talks[saal])
+        logging.debug("f_talk: %s", f_talk)
+        for saal in f_talk:
+            if f_talk[saal]:
+                client.write(0,i,u'{} => {} : {:<78}'.format(saal,f_talk[saal]['start'].encode('utf-8'), f_talk[saal]["title"].encode('utf-8')))
+            i = i + 1
+        time.sleep(0.5)
+        tick += 1
---
+++ python/client3.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import bitmapfont
+
+SPLIT_N = 1
+SPLIT_I = 0
+
+SPLIT = os.environ.get("SPLIT")
+if SPLIT:
+  SPLIT = map(int, SPLIT.split('/'))
+  SPLIT_N = SPLIT[1]
+  SPLIT_I = SPLIT[0]-1
+
+NUM_SEG_X = 96
+NUM_SEG_Y = 10
+PWIDTH = 5
+WIDTH = PWIDTH*NUM_SEG_X//SPLIT_N
+WOFFSET = WIDTH*SPLIT_I
+PHEIGHT = 7
+PPAD = 5
+HEIGHT = PHEIGHT*NUM_SEG_Y
+
+import sys
+
+SERVER = "tcp://mensadisplay:5556"
+
+if len(sys.argv) >= 2:
+  SERVER = sys.argv[1]
+
+import struct
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect(SERVER)
+
+################################################################################
+# RAW PIXEL HANDLING
+################################################################################
+
+def set_pixel(x, y, v):
+  x += WOFFSET
+  tx = struct.pack('<BiiB', 0, x, y, v)
+  socket.send_multipart([tx, b''])
+  rx = socket.recv()
+  #print("Received reply %s [%r]" % ((x, y, v), rx))
+
+def set_pixels(pixels):
+  msg = []
+  for x, y, v in pixels:
+    x += WOFFSET
+    msg.append(struct.pack('<BiiB', 0, x, y, v))
+  socket.send_multipart(msg + [b''])
+  rx = socket.recv()
+
+def blit(x, y, w, h, pixels, rec=True):
+  x += WOFFSET
+  assert w*h == len(pixels)
+  msg = struct.pack('<Biiii', 1, x, y, w, h)+b''.join(map(chr, pixels))
+  socket.send_multipart([msg, b''])
+  if rec:
+    rx = socket.recv()
+
+def rec():
+  rx = socket.recv()
+
+################################################################################
+# TEXT RENDERING WITH BITMAP FONT
+################################################################################
+
+# screen buffer used for text handling
+SCREENBUFFER = [0] * WIDTH * HEIGHT
+
+# blit to screen buffer, also updates screen
+def screenbuf_blit(x, y, w, h, pixels):
+    blit(x, y, w, h, pixels)
+    assert w*h == len(pixels)
+    for dy in range(0, h):
+        for dx in range(0, w):
+            if(y+dy >= HEIGHT or x+dx >= WIDTH):
+                return
+            pix = pixels[dy * w + dx];
+            SCREENBUFFER[(y+dy) * WIDTH + (x+dx)] = pix
+
+# scroll the screen buffer up y pixels, also updates screen
+def screenbuf_scroll(y):
+    buf = SCREENBUFFER
+    for dy in range(y, HEIGHT):
+        for x in range(0, WIDTH):
+            SCREENBUFFER[(dy-y) * WIDTH + x] = buf[dy * WIDTH + x]
+    screenbuf_render()
+
+# draw screen buffer to screen
+def screenbuf_render():
+    blit(0, 0, WIDTH, HEIGHT, SCREENBUFFER)
+
+# return array of size PWIDTH * PHEIGHT (indexed by row, then column)
+def char_to_pixel_segment(c):
+    pixels = [0] * PWIDTH * PHEIGHT
+
+    if(c not in bitmapfont.FONT.keys()):
+        c = u"☐";
+
+    for x in xrange(0, PWIDTH):
+        for y in xrange(0, PHEIGHT):
+            pix = (bitmapfont.FONT[c][x] & (1<<y)) >> y
+            pixels[y * PWIDTH + x] = pix * 255
+    return pixels
+
+# write string, starting at segment x,y. Tabs are expanded to 8 spaces, new
+# lines always begin at the given x position. No boundary checks are done, text
+# may be clipped at the border, in this case the function returns.
+# This function returns a tuple (x,y,success) where (x,y) gives the position of
+# the last character written, and success is set to False if the function
+# returned because of clipped text.
+def write(x, y, string):
+    orig_x = x
+    string = string.replace("\t", " "*8)
+    for c in string:
+        if c == "\n":
+            y += 1
+            x = orig_x
+        if ord(c) < 0x1f:
+            pass
+        else:
+            pixels = char_to_pixel_segment(c)
+            screenbuf_blit(x*PWIDTH, y*PHEIGHT, PWIDTH, PHEIGHT, pixels)
+            x += 1
+
+        if(x > NUM_SEG_X):
+            return (x,y,False)
+
+    return (x,y,True)
+
+# write line to screen as if on a terminal, scroll up if neccessary
+cur_line = 0
+def writeline(string):
+    global cur_line
+    if(cur_line >= NUM_SEG_Y):
+        scrollline()
+        cur_line -= 1
+    (new_x, new_y, success) = write(0, cur_line, string.strip("\r\n"))
+    cur_line = new_y
+
+    # clear remaining row
+    clear_chars = (NUM_SEG_X-new_x)
+    screenbuf_blit(new_x * PWIDTH, cur_line * PHEIGHT,
+        clear_chars * PWIDTH, PHEIGHT,
+        [0] * clear_chars * PWIDTH * PHEIGHT)
+
+    cur_line += 1
+
+# scroll the content y lines up and clear last line
+def scrollline(y = 1):
+    screenbuf_scroll(y*PHEIGHT)
+    screenbuf_blit(0, (NUM_SEG_Y-1) * PHEIGHT, WIDTH, PHEIGHT,
+        [0] * WIDTH * PHEIGHT)
