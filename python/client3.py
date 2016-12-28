fd5f9f8c22f0d50d1f5408de86cbd9f15abd6de7
Emantor <phoenix@emantor.de>
2016-12-28 16:47:17 +0100

    client3: Fix struct bytes

--- python/client3.py
+++ python/client3.py
@@ -42,7 +42,7 @@ socket.connect(SERVER)
 
 def set_pixel(x, y, v):
   x += WOFFSET
-  tx = struct.pack('<BiiB', 0, x, y, v)
+  tx = struct.pack(b'<BiiB', 0, x, y, v)
   socket.send_multipart([tx, b''])
   rx = socket.recv()
   #print("Received reply %s [%r]" % ((x, y, v), rx))
@@ -51,14 +51,14 @@ def set_pixels(pixels):
   msg = []
   for x, y, v in pixels:
     x += WOFFSET
-    msg.append(struct.pack('<BiiB', 0, x, y, v))
+    msg.append(struct.pack(b'<BiiB', 0, x, y, v))
   socket.send_multipart(msg + [b''])
   rx = socket.recv()
 
 def blit(x, y, w, h, pixels, rec=True):
   x += WOFFSET
   assert w*h == len(pixels)
-  msg = struct.pack('<Biiii', 1, x, y, w, h)+b''.join(map(chr, pixels))
+  msg = struct.pack(b'<Biiii', 1, x, y, w, h)+b''.join(map(chr, pixels))
   socket.send_multipart([msg, b''])
   if rec:
     rx = socket.recv()
